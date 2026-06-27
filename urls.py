63f3a5f377e7739d90bd949444ca6c6d982b1803
vasti <vati050606@gmail.com>
2026-06-26 18:01:14 -0600

    Agregar .python-version para Vercel

--- urls.py
+++ urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tareas.urls')),  # 👈 AGREGAR ESTA LÍNEA
+    path('', include('tareas.urls')),
 ]
--- wsgi.py
+++ wsgi.py
@@ -10,8 +10,6 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-# ✅ CORRECTO: settings.py está en la raíz
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
-
 application = get_wsgi_application()
-app = application  # Para Vercel
+app = application
